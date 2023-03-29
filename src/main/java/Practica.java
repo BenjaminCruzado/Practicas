d0ffe83a2f7d0581f731284a9fbf6870728a6e2d
BenjaminCruzado <b.cruzado01@ufromail.cl>
2023-03-29 20:43:40 -0300

    Subo metodo elFinal.

--- src/main/java/Practica.java
+++ src/main/java/Practica.java
@@ -1,5 +1,12 @@
 public class Practica {
+    public static void main(String[] args){
+        hola();
+        elFinal();
+    }
     public static void hola(){
         System.out.println("Hola");
+    }
+    public static void elFinal(){
+        System.out.println("Final");
     }
 }
